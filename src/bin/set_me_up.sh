9975bce87868fb600554cf473b9f14202694bdf8
Martin VanWinkle III <mvanwinkle@ias.edu>
2021-03-18 16:42:29 -0400

    progress

---
+++ src/bin/set_me_up.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+script_dir=$(realpath $( dirname "$0" ) )
+
+echo "Script dir: $script_dir"
+two_up=$(basename $( dirname "$script_dir"))
+etc_dir=""
+echo "2 up: $two_up"
+
+if [[ "$two_up" == "src" ]]
+then
+	etc_dir=$(realpath "${script_dir}"/../etc)
+	root_etc_dir=$(realpath "${script_dir}"/../root_etc/IAS/mv-bashrc-d)
+else
+	artifact_name=$(basename "$script_dir")
+	etc_dir=$(realpath "${script_dir}/../../etc/${artifact_name}")
+	root_etc_dir="/etc/IAS/${artifact_name}/"
+fi
+
+echo "etc_dir: $etc_dir"
+echo "root_etc_dir: $root_etc_dir"
+
+ln -s "${root_etc_dir}" ~/.bashrc.d
+cat "${etc_dir}/bashrc" >> ~/.bashrc
+
